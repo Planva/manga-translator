f4896b750ac1f2471ae83c2b259697ed32d39ac3
Planva <planvaofficial@gmail.com>
2025-07-31 21:08:33 +0800

    fix: replace next.config.mjs with CommonJS next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    // 开启静态导出
+    output: 'export',
+    // 如需每个页面生成到 /page-name/index.html，取消下一行注释：
+    // trailingSlash: true,
+  };
+  
