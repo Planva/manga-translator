cdffc1ae37acdee4e0e3e0b92cbd33c0378255d6
Planva <planvaofficial@gmail.com>
2025-07-31 21:20:12 +0800

    first

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+// postcss.config.cjs
+module.exports = {
+    plugins: {
+      // 如果你有自定义插件顺序，也可以写成数组
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
+  
--- postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-module.exports = {
-  // 开启静态导出
-  output: 'export',
-  // 如果你希望每个页面生成到 /page-name/index.html，请取消注释下面一行：
-  // trailingSlash: true,
-};
