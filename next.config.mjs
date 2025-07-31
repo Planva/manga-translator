c3d0a5c6a8123bdfa1383fbdacf06dd99dc58e00
Planva <planvaofficial@gmail.com>
2025-07-31 21:03:45 +0800

    first

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
     // 如果你想所有页面都输出到 /page-name/index.html：
     // trailingSlash: true,
   };
+module.exports = {
+output: 'export',
+// 如需每个页面生成到 `page-name/index.html`，再加下面这一行：
+// trailingSlash: true,
+}
   
   export default nextConfig;
   
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// next.config.js
+/** @type {import('next').NextConfig} */
+module.exports = {
+  // 开启静态导出
+  output: 'export',
+  // 如果你希望每个页面生成到 /page-name/index.html，请取消注释下面一行：
+  // trailingSlash: true,
 };
-
-export default config;
-
-
-
