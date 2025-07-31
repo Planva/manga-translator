5b873d437cb36a03e20365889974d9ec6a4074c7
Planva <planvaofficial@gmail.com>
2025-07-31 20:46:16 +0800

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // 开启静态导出
+    output: 'export',
+    // 如果你想所有页面都输出到 /page-name/index.html：
+    // trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
